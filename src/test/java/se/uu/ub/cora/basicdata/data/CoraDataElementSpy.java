8312cb2a08da5842e2667ce55f128e3d7445c349
olovm <olov.mckie@ub.uu.se>
2022-03-31 12:31:59 +0000

    Updated for cora-data 6.0.0

--- src/test/java/se/uu/ub/cora/basicdata/data/CoraDataElementSpy.java
+++ src/test/java/se/uu/ub/cora/basicdata/data/CoraDataElementSpy.java
@@ -1,5 +1,8 @@
 package se.uu.ub.cora.basicdata.data;
 
+import java.util.Collection;
+
+import se.uu.ub.cora.data.DataAttribute;
 import se.uu.ub.cora.data.DataElement;
 
 public class CoraDataElementSpy implements DataElement {
@@ -20,6 +23,30 @@ public class CoraDataElementSpy implements DataElement {
 	public void setRepeatId(String repeatId) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void addAttributeByIdWithValue(String nameInData, String value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean hasAttributes() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public DataAttribute getAttribute(String nameInData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<DataAttribute> getAttributes() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/test/java/se/uu/ub/cora/basicdata/data/DataGroupSpy.java
+++ src/test/java/se/uu/ub/cora/basicdata/data/DataGroupSpy.java
@@ -170,4 +170,28 @@ public class DataGroupSpy implements DataGroup {
 		childDataGroupToReturn.throwException = true;
 	}
 
+	@Override
+	public void addAttributeByIdWithValue(String nameInData, String value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean hasAttributes() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public DataAttribute getAttribute(String nameInData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<DataAttribute> getAttributes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
